9200d8f88fdfa7642323ef2a7d2f0c2f7bde498d
gutovesco <guto.vesco@hotmail.com>
2020-06-03 15:17:14 -0300

    databse, migrations, get items route and post points route

--- src/database/migrations/00_create_points.ts
+++ src/database/migrations/00_create_points.ts
@@ -15,5 +15,5 @@ export async function up(knex: Knex){
 }
 
 export async function down(knex: Knex){
-    return knex.schema.dropSchema('point')
+    return knex.schema.dropSchema('points')
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,46 @@
 import express from 'express';
+import knex from './database/connection';
 
-const routes = express.Router()
+const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json({message: 'hello'})   
-}
-)
+routes.get('/items', async (request, response) => {
+    const items = await knex('items').select('*');
 
-export default routes
+    const serializedItems = items.map(item => {
+        return {
+            id: item.id,
+            title: item.title,
+            image_url: `http://localhost:3330/uploads/${item.image}`
+        }
+    })
+    
+    return response.json(serializedItems)   
+});
+
+routes.post('/points', async(request, response) => {
+    const {name, email, whatsapp, latitude, longitude, city, uf, items} = request.body;
+
+   const ids = await knex('points').insert({
+        image: 'image-fake',
+        name, 
+        email, 
+        whatsapp, 
+        latitude, 
+        longitude, 
+        city, 
+        uf,
+    })
+
+    const pointItems = items.map((item_id: number) => {
+        return {
+            item_id,
+            point_id: ids[0]
+        }
+    })
+
+    await knex('point_items').insert(pointItems)
+
+    return response.json({success: true})
+});
+
+export default routes;
